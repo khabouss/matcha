4569516658c9251327000e1da21ab8c8c8285832
Mohammed Amine Ajdahim <amine.ajdahim.16@gmail.com>
2024-10-11 17:54:07 +0100

    add prisma with postgress

--- backend/src/Modules/Auth/Routes/Auth.route.ts
+++ backend/src/Modules/Auth/Routes/Auth.route.ts
@@ -1,13 +1,26 @@
-import { Router } from "express";
+import { Router } from 'express';
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 const attachAuthRoute = (router: Router) => {
-  router.post("/login", (req, res) => {
-    res.send("Login page");
-  });
+    router.post('/login', (req, res) => {
+        res.send('Login page');
+    });
 
-  router.post("/register", (req, res) => {
-    res.send("Register page");
-  });
+    router.post('/register', (req, res) => {
+        res.send('Register page');
+    });
+    router.post('/users', async (req, res) => {
+        const { name, email } = req.body;
+        const user = await prisma.user.create({
+            data: { name, email },
+        });
+        res.json(user);
+    });
+    router.get('/users', async (req, res) => {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    });
 };
 
 export default attachAuthRoute;
