9fd9f3c92853f5c9ddb02d22be5320e16fbcd828
Mohammed Amine Ajdahim <amine.ajdahim.16@gmail.com>
2024-10-25 22:44:03 +0100

    edit profile

--- backend/src/Modules/Auth/Controllers/profiles/GetProfile.ts
+++ backend/src/Modules/Auth/Controllers/profiles/GetProfile.ts
@@ -35,14 +35,38 @@ const GetProfileUsers = async (
         }
 
         const { user_id, gps_location, allow_gps, ...returndata } = profile;
-
+        const resData = {
+            name: 'Alice',
+            isMatch: true,
+            lastOnlineStatus: 'Two weeks ago',
+            about: {
+                gender: 'Man',
+                SP: 'Straight',
+                interest1: 'Jogging',
+                interest2: 'Coffee',
+                intereset3: 'Movies',
+                fame: 'fame: 3.12',
+            },
+            languages: ['english', 'arabic', 'russian'],
+            lastLocation: 'Marrakech',
+            age: 25,
+            bio: 'Love traveling and photography.',
+            image: [
+                'https://picsum.photos/400/600?random=1',
+                'https://picsum.photos/400/600?random=1',
+                'https://picsum.photos/400/600?random=1',
+                'https://picsum.photos/400/600?random=1',
+                'https://picsum.photos/400/600?random=1',
+            ],
+            offsetX: 0,
+            rotate: 0,
+            opacity: 1,
+        };
         res.status(200).json({
             status: 'success',
             data: {
                 message: 'Profile fetched successfully!',
-                data: {
-                    ...returndata,
-                },
+                profile: resData,
             },
         });
     } catch (error: any) {
